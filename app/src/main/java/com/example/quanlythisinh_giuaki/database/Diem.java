9f2182cc8480582f8adbc959f2ff3fbdb33bd040
tailouis <vuductaiptit@gmail.com>
2019-05-01 10:23:30 +0700

    setup database

---
+++ app/src/main/java/com/example/quanlythisinh_giuaki/database/Diem.java
@@ -0,0 +1,80 @@
+package com.example.quanlythisinh_giuaki.database;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+public class Diem {
+
+    public static final String TABLE_NAME = "diem";
+    public static final String COLUMN_MASV = "masv";
+    public static final String COLUMN_MAMON = "mamon";
+    public static final String COLUMN_DIEM = "diem";
+
+    private int masv;
+    private String tenSv;
+    private String tenMonhoc;
+    private int mamon;
+    private float diem;
+
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_MASV + "INT,"
+                    + COLUMN_MAMON + "INT,"
+                    + COLUMN_DIEM + "FLOAT,"
+                    + "PRIMARY KEY AUTOINCREMENT (" + COLUMN_MASV + "," + COLUMN_MAMON + ")"
+                    + ")";
+
+    public Diem() {
+    }
+
+    public Diem(int masv, int mamon, float diem) {
+        this.mamon = mamon;
+        this.masv = masv;
+        this.diem = diem;
+    }
+
+    public int getMasv() {
+        return masv;
+    }
+
+    public void setMasv(int masv) {
+        this.masv = masv;
+    }
+
+    public int getMamon() {
+        return mamon;
+    }
+
+    public void setMamon(int mamon) {
+        this.mamon = mamon;
+    }
+
+    public float getDiem() {
+        return diem;
+    }
+
+    public void setDiem(float diem) {
+        this.diem = diem;
+    }
+
+    public String getTenSv() {
+        return tenSv;
+    }
+
+    public void setTenSv(String tenSv) {
+        this.tenSv = tenSv;
+    }
+
+    public String getTenMonhoc() {
+        return tenMonhoc;
+    }
+
+    public void setTenMonhoc(String tenMonhoc) {
+        this.tenMonhoc = tenMonhoc;
+    }
+
+}
---
+++ app/src/main/java/com/example/quanlythisinh_giuaki/database/Mon.java
@@ -0,0 +1,51 @@
+package com.example.quanlythisinh_giuaki.database;
+
+public class Mon {
+
+    public static final String TABLE_NAME = "mon";
+    public static final String COLUMN_TENMON = "tenmon";
+    public static final String COLUMN_MAMON = "mamon";
+    public static final String COLUMN_SOTIET = "sotiet";
+
+    private String tenmon;
+    private String mamon;
+    private int sotiet;
+
+    public Mon(String tenmon, String mamon, int sotiet) {
+        this.tenmon = tenmon;
+        this.mamon = mamon;
+        this.sotiet = sotiet;
+    }
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_MAMON + " INTEGER PRIMARY KEY,"
+                    + COLUMN_TENMON + " TEXT,"
+                    + COLUMN_SOTIET + " INTEGER,"
+                    + ")";
+
+    public String getTenmon() {
+        return tenmon;
+    }
+
+    public void setTenmon(String tenmon) {
+        this.tenmon = tenmon;
+    }
+
+    public String getMamon() {
+        return mamon;
+    }
+
+    public void setMamon(String mamon) {
+        this.mamon = mamon;
+    }
+
+    public int getSotiet() {
+        return sotiet;
+    }
+
+    public void setSotiet(int sotiet) {
+        this.sotiet = sotiet;
+    }
+}
